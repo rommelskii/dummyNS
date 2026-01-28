5d0e6edfbcef1a29c9801b4382f7a2e6044af170
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-01-29 00:40:51 +0800

    [config] added boilerplate for configuration program

--- src/dns_config.cpp
+++ src/dns_config.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <cstring>
+#include <fstream>
+#include <filesystem>
 
-int main() 
+#define MAXARGLEN 256
+
+int main(int argc, char* argv[]) 
 {
-  std::cout << "Config hello\n";
+  if (argc < 2)   
+  {
+    std::cerr << "Argument error: lacking argument" << "\n";
+    exit(EXIT_FAILURE);
+  }
+
+  const size_t arglen = strnlen(argv[1], MAXARGLEN);
+
+  if (arglen >= MAXARGLEN) 
+  {
+    std::cerr << "Argument error: argument too large" << "\n";
+    exit(EXIT_FAILURE);
+  }
+
+  char arg_buf[MAXARGLEN];
+  memset(arg_buf, 0, sizeof(arg_buf));
+
+  memcpy(arg_buf, argv[1], arglen);
+
+
+  //insert configuration file logic here for loading and replacing...
+
+
 
   exit(EXIT_SUCCESS);
 }
