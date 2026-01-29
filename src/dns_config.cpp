d4f05f82dc786c213e95bd61a5b7d280a132b8a7
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-01-30 03:13:16 +0800

    [config] added PoC code

--- src/dns_config.cpp
+++ src/dns_config.cpp
@@ -1,35 +1,103 @@
 #include <iostream>
 #include <cstring>
 #include <fstream>
+#include <cstdlib>
 #include <filesystem>
 
-#define MAXARGLEN 256
+namespace fs = std::filesystem;
+const char* HOME = std::getenv("HOME");
+
+#define MAXARGLEN 1024
+
+std::filesystem::path fix_path(char* path) 
+{
+  if (path[0] == '/') // path provided is absolute
+  {
+    return std::filesystem::path(path);
+  }
+  // otherwise, path is relative and must be appended to the absolute current path
+  std::filesystem::path normalized_path = std::filesystem::current_path() / path;
+  return normalized_path.lexically_normal();
+}
 
 int main(int argc, char* argv[]) 
 {
+  size_t arglen;
   if (argc < 2)   
   {
     std::cerr << "Argument error: lacking argument" << "\n";
+    std::cout << "Usage: ./dns_config (create | config) (config_path)" << "\n";
     exit(EXIT_FAILURE);
   }
-
-  const size_t arglen = strnlen(argv[1], MAXARGLEN);
-
-  if (arglen >= MAXARGLEN) 
+  else if (argc == 3)
   {
-    std::cerr << "Argument error: argument too large" << "\n";
-    exit(EXIT_FAILURE);
+    arglen = strnlen(argv[2], MAXARGLEN);
+    if (arglen >= MAXARGLEN) 
+    {
+      std::cerr << "Argument error: argument too large" << "\n";
+      exit(EXIT_FAILURE);
+    }
   }
 
-  char arg_buf[MAXARGLEN];
-  memset(arg_buf, 0, sizeof(arg_buf));
+  char type_buf[MAXARGLEN];
+  char pconfig_buf[MAXARGLEN];
+  char line_buf[1024];
 
-  memcpy(arg_buf, argv[1], arglen);
+  memset(type_buf, 0, sizeof(type_buf));
+  memset(line_buf, 0, sizeof(line_buf));
+  memset(pconfig_buf, 0, sizeof(pconfig_buf));
+
+  size_t type_len = strnlen(argv[1], 1024);
+  memcpy(type_buf, argv[1], type_len);
 
 
-  //insert configuration file logic here for loading and replacing...
+  for (int i=0; i<type_len; ++i) 
+  {
+    type_buf[i] = tolower(type_buf[i]);
+  }
 
+  std::filesystem::path pconfig_root = fs::path(HOME) / ".config" / "dummyns" / "config.dummy";
+  if ( strncmp(type_buf, "create", 1024) == 0 ) 
+  {
+    std::fstream config(pconfig_root, std::ios::out);
+    if (!fs::exists(pconfig_root)) 
+    {
+      std::cout << "Default configuration file does not exist. Creating...";
+    }
+    if (!config.is_open()) 
+    {
+      std::cerr << "File error: failed to create default configuration file at " << pconfig_root << "\n";
+      exit(EXIT_FAILURE);
+    }
+    config << "default";
+    std::cout << "Created configuration file at " << pconfig_root << "\n";
+  }
+  else if ( strncmp(type_buf, "custom", 1024) == 0 )
+  {
+    if (argc < 3)
+    {
+      std::cerr << "Argument error: specify the path to the configuration file" << "\n";
+      std::cout << "Usage: ./dns_config (create | config) (config_path)" << "\n";
+      exit(EXIT_FAILURE);
+    }
+    
+    size_t custom_path_length = strnlen(argv[2], 1024);
+    memcpy(pconfig_buf, argv[2], custom_path_length);
+    std::filesystem::path fixed_path = fix_path(pconfig_buf);
+    
+    std::fstream file(pconfig_root, std::ios::in | std::ios::out);
+    if (!file.is_open()) 
+    {
+      std::cerr << "File error: custom configuration does not exist" << "\n";
+      exit(EXIT_FAILURE);
+    }
+
+    file << fixed_path;
+
+    std::cout << "Hosts file set to " << fixed_path << "\n";
+  }
 
 
   exit(EXIT_SUCCESS);
 }
+
